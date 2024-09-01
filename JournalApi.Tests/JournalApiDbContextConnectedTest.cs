d3b7d35addf939ad1316ce4c56a709982670e8a5
Rumcajs121 <z.miara45@gmail.com>
2024-09-01 16:58:37 +0200

    feat(db/memorychache)I use memory cache for my get call weatherforecast. I not complete my test connection db

--- JournalApi.Tests/JournalApiDbContextConnectedTest.cs
+++ JournalApi.Tests/JournalApiDbContextConnectedTest.cs
@@ -1,19 +1,38 @@
 using Journal.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 namespace JournalApi.Tests;
 
-public class JournalApiDbContextConnectedTest
+public class JournalApiDbContextConnectedTest:IClassFixture<JournalApiDbContextFixture>
 {
     private readonly JournalDbContext _dbContext;
 
-    public JournalApiDbContextConnectedTest(JournalDbContext dbContext)
+    public JournalApiDbContextConnectedTest(JournalApiDbContextFixture fixture)
     {
-        _dbContext = dbContext;
+        _dbContext = fixture.DbContext;
     }
     [Fact]
     public void AddInfrastructure_CanConnectedDb_IsConnected()
     {
         var connect=_dbContext.Database.CanConnect();
         Assert.True(connect, "Db is not connect");
+    }
+}
+public class JournalApiDbContextFixture : IDisposable
+{
+    public JournalDbContext DbContext { get; private set; }
+
+    public JournalApiDbContextFixture()
+    {
+        var options = new DbContextOptionsBuilder<JournalDbContext>()
+            .UseSqlServer("Connectionstring:JournalApiDb")
+            .Options;
+
+        DbContext = new JournalDbContext(options);
+    }
+
+    public void Dispose()
+    {
+        DbContext.Dispose();
     }
 }
--- JournalApi/Controllers/WeatherForecastController.cs
+++ JournalApi/Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
 using System.Net.Sockets;
 using JournalApi.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace JournalApi.Controllers;
 
@@ -10,35 +11,56 @@ public class WeatherForecastController : ControllerBase
 {
     private readonly ILogger<WeatherForecastController> _logger;
     private readonly IWeatherForecastService _service;
+    private readonly IMemoryCache _memoryCache;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService service)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService service,
+        IMemoryCache memoryCache)
     {
         _logger = logger;
         _service = service;
+        _memoryCache = memoryCache;
     }
 
     [HttpGet]
     [Route("{take:int}/example")]
-    public ObjectResult Get([FromQuery] int max, [FromRoute] int take)
+    public async Task<ObjectResult> Get([FromQuery] int max, [FromRoute] int take)
     {
-        var result=_service.Get();
-        return Ok(result);
+        var sepamphore = new SemaphoreSlim(1, 1);
+        if (!_memoryCache.TryGetValue("forecast", out IEnumerable<WeatherForecast>? forecast))
+        {
+            try
+            {
+                await sepamphore.WaitAsync();
+                if (!_memoryCache.TryGetValue("forecast", out forecast))
+                {
+                    forecast = _service.Get();
+                    _memoryCache.Set("forecast", forecast, 
+                        new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(10)));
+                }
+            }
+            finally
+            {
+                sepamphore.Release();
+            }
+        }
 
+        return Ok(forecast);
     }
+
     [HttpPost("generate")]
-    public IActionResult Generate([FromQuery] int take,[FromBody]TemperatureRequest request)
+    public IActionResult Generate([FromQuery] int take, [FromBody] TemperatureRequest request)
     {
         if (take < 0 || request.MaxTemperature < request.MinTemperature)
         {
             return BadRequest("Error");
         }
-        var result=_service.ExerciseGet(request.MaxTemperature,request.MinTemperature,take);
-        return Ok(result);
-        
 
+        var result = _service.ExerciseGet(request.MaxTemperature, request.MinTemperature, take);
+        return Ok(result);
     }
-    [HttpPost(Name="AddWeatherForecast")]
-    public ObjectResult Add ([FromBody] WeatherForecast weatherForecast)
+
+    [HttpPost(Name = "AddWeatherForecast")]
+    public ObjectResult Add([FromBody] WeatherForecast weatherForecast)
     {
         var weatherNewForPlace = new WeatherForecast
         {
@@ -46,9 +68,8 @@ public class WeatherForecastController : ControllerBase
             TemperatureC = weatherForecast.TemperatureC,
             Summary = weatherForecast.Summary
         };
-        return StatusCode(200,weatherNewForPlace);
+        return StatusCode(200, weatherNewForPlace);
     }
-
 }
 
 public class TemperatureRequest
